93fc013591e776e1d7bfa6ed7f98e3867272c921
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2014-12-05 00:45:24 +0100

    First unit test of the app delegate.

---
+++ BadHunterTests/AppDelegateTests.m
@@ -0,0 +1,57 @@
+//
+//  AppDelegateTests.m
+//  BadHunter
+//
+//  Created by Jorge D. Ortiz Fuentes on 5/12/14.
+//  Copyright (c) 2014 PoWWaU. All rights reserved.
+//
+
+
+#import <XCTest/XCTest.h>
+//#import <OCMock/OCMock.h>
+#import "AppDelegate.h"
+
+
+@interface AppDelegateTests : XCTestCase {
+    // Object to test.
+    AppDelegate *sut;
+}
+
+@end
+
+
+@implementation AppDelegateTests
+
+#pragma mark - Set up and tear down
+
+- (void) setUp {
+    [super setUp];
+
+    [self createSut];
+}
+
+
+- (void) createSut {
+    sut = [[AppDelegate alloc] init];
+}
+
+
+- (void) tearDown {
+    [self releaseSut];
+
+    [super tearDown];
+}
+
+
+- (void) releaseSut {
+    sut = nil;
+}
+
+
+#pragma mark - Basic test
+
+- (void) testObjectIsNotNil {
+    XCTAssertNotNil(sut, @"The object to test must be created in setUp.");
+}
+
+@end
