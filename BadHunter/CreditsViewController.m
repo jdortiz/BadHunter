17623f13b7fcd1dd4ec920b8e43b6a387e566a04
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-12 15:18:59 +0200

    Animate the icon with 2D transforms.

--- BadHunter/CreditsViewController.m
+++ BadHunter/CreditsViewController.m
@@ -29,24 +29,21 @@ static NSTimeInterval iconAnimationDuration = 0.5;
 - (void) viewDidLoad {
     [super viewDidLoad];
     self.iconImageView.alpha = 0.0;
+//    self.iconImageView.transform = CGAffineTransformMakeScale(0.1, 0.1);
+//    self.iconImageView.transform = CGAffineTransformScale(CGAffineTransformMakeRotation(M_PI), 0.1, 0.1);
+    self.iconImageView.transform = CGAffineTransformScale(CGAffineTransformRotate(CGAffineTransformMakeTranslation(-100.0, -100), M_PI), 0.1, 0.1);
+//    self.iconImageView.transform = CGAffineTransformTranslate(CGAffineTransformRotate(CGAffineTransformMakeScale(0.1, 0.1), M_PI), -100.0, -100.0);
 }
 
 
 - (void) viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     if (animated) {
-//        [UIView animateWithDuration:iconAnimationDuration animations:^{
-//            self.iconImageView.alpha = 1.0;
-//        }];
-//        [UIView animateWithDuration:iconAnimationDuration delay:0.0
-//                            options:UIViewAnimationOptionCurveEaseInOut
-//                         animations:^{
-//                             self.iconImageView.alpha = 1.0;
-//                         } completion:nil];
         [UIView animateWithDuration:iconAnimationDuration delay:0.0
              usingSpringWithDamping:1 initialSpringVelocity:0.1
                             options:0 animations:^{
                                 self.iconImageView.alpha = 1.0;
+                                self.iconImageView.transform = CGAffineTransformIdentity;
                             } completion:nil];
     }
 }
