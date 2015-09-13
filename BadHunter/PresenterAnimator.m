5194ab3a4904a3a3ec90eaa7db4d2d7bffb9b7d4
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-13 15:42:20 +0200

    Animate the backwards transition with snapshots and 3D transforms.

--- BadHunter/PresenterAnimator.m
+++ BadHunter/PresenterAnimator.m
@@ -17,10 +17,16 @@
 @end
 
 
+
 @implementation PresenterAnimator
 
-static const NSTimeInterval duration = 0.4;
+#pragma mark - Constants & Parameters
 
+static const NSTimeInterval duration = 0.4;
+static const float perspectiveFactor = -1/500.0;
+
+
+#pragma mark - Initialization
 
 - (instancetype) initForwardTransition:(BOOL)forward {
     if (self = [super init]) {
@@ -30,6 +36,8 @@ static const NSTimeInterval duration = 0.4;
     return self;
 }
 
+
+#pragma mark - View Controller Animated Transitioning
 
 - (void) animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
     UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
@@ -73,7 +81,6 @@ static const NSTimeInterval duration = 0.4;
         bottomHalfImage.layer.position = CGPointMake(viewSize.width / 2.0, viewSize.height);
         [UIView animateWithDuration:duration delay:0.0
                             options:0 animations:^{
-                                static const float perspectiveFactor = -1/500.0;
                                 CATransform3D rotate3DUp = CATransform3DMakeRotation(M_PI_2, 1.0, 0.0, 0.0);
                                 rotate3DUp.m34 = perspectiveFactor;
                                 topHalfImage.layer.transform = rotate3DUp;
@@ -94,15 +101,46 @@ static const NSTimeInterval duration = 0.4;
 - (void) animateTransitionBackwardWithContext:(id<UIViewControllerContextTransitioning>)context
                            fromViewController:(UIViewController *)fromViewController toViewController:(UIViewController *)toViewController
                                      fromView:(UIView *)fromView toView:(UIView *)toView {
+    static const float perspectiveFactor = -1/500.0;
     UIView *containerView = [context containerView];
     toView.frame = [context finalFrameForViewController:toViewController];
     if ([context isAnimated]) {
-        CGRect endFrame = CGRectMake(0.0, -containerView.frame.size.height, containerView.frame.size.width, containerView.frame.size.height);
+        CGSize viewSize = toView.frame.size;
+        [containerView addSubview:toView];
+
+        CGRect topFrame = CGRectMake(0.0, 0.0, viewSize.width, viewSize.height/2);
+        CGRect bottomFrame = CGRectMake(0.0, viewSize.height/2, viewSize.width, viewSize.height/2);
+        UIView *topHalfImage = [toView resizableSnapshotViewFromRect:topFrame
+                                                  afterScreenUpdates:YES withCapInsets:UIEdgeInsetsZero];
+        topHalfImage.frame = topFrame;
+        UIView *bottomHalfImage = [toView resizableSnapshotViewFromRect:bottomFrame
+                                                     afterScreenUpdates:YES withCapInsets:UIEdgeInsetsZero];
+        bottomHalfImage.frame = bottomFrame;
+
+        [fromView addSubview:topHalfImage];
+        topHalfImage.layer.anchorPoint = CGPointMake(0.5, 0.0);
+        topHalfImage.layer.position = CGPointMake(viewSize.width / 2.0, 0.0);
+        [fromView addSubview:bottomHalfImage];
+        bottomHalfImage.layer.anchorPoint = CGPointMake(0.5, 1.0);
+        bottomHalfImage.layer.position = CGPointMake(viewSize.width / 2.0, viewSize.height);
+        [toView removeFromSuperview];
+
+        CATransform3D rotate3DUp = CATransform3DMakeRotation(M_PI_2, 1.0, 0.0, 0.0);
+        rotate3DUp.m34 = perspectiveFactor;
+        topHalfImage.layer.transform = rotate3DUp;
+        CATransform3D rotate3DDown = CATransform3DMakeRotation(-M_PI_2, 1.0, 0.0, 0.0);
+        rotate3DDown.m34 = perspectiveFactor;
+        bottomHalfImage.layer.transform = rotate3DDown;
+
         [containerView insertSubview:toView belowSubview:fromView];
         [UIView animateWithDuration:duration delay:0.0
                             options:0 animations:^{
-                                fromView.frame = endFrame;
+                                topHalfImage.layer.transform = CATransform3DIdentity;
+                                bottomHalfImage.layer.transform = CATransform3DIdentity;
                             } completion:^(BOOL finished) {
+                                [containerView addSubview:toView];
+                                [topHalfImage removeFromSuperview];
+                                [bottomHalfImage removeFromSuperview];
                                 [context completeTransition:YES];
                             }];
     } else {
