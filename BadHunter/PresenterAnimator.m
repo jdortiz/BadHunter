efe6d78fda071e8ffcf7e479b09c75c8e3fe24c0
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-13 14:59:48 +0200

    Animate the forward transition using snapshots and 3D.

--- BadHunter/PresenterAnimator.m
+++ BadHunter/PresenterAnimator.m
@@ -52,19 +52,40 @@ static const NSTimeInterval duration = 0.4;
                           fromViewController:(UIViewController *)fromViewController toViewController:(UIViewController *)toViewController
                                     fromView:(UIView *)fromView toView:(UIView *)toView {
     UIView *containerView = [context containerView];
+    toView.frame = [context finalFrameForViewController:toViewController];
     if ([context isAnimated]) {
-        CGRect startFrame = CGRectMake(0.0, -containerView.frame.size.height, containerView.frame.size.width, containerView.frame.size.height);
-        toView.frame = startFrame;
+        CGSize viewSize = fromView.frame.size;
+        CGRect topFrame = CGRectMake(0.0, 0.0, viewSize.width, viewSize.height/2);
+        CGRect bottomFrame = CGRectMake(0.0, viewSize.height/2, viewSize.width, viewSize.height/2);
+        UIView *topHalfImage = [fromView resizableSnapshotViewFromRect:topFrame
+                                                    afterScreenUpdates:NO withCapInsets:UIEdgeInsetsZero];
+        topHalfImage.frame = topFrame;
+        UIView *bottomHalfImage = [fromView resizableSnapshotViewFromRect:bottomFrame
+                                                       afterScreenUpdates:NO withCapInsets:UIEdgeInsetsZero];
+        bottomHalfImage.frame = bottomFrame;
+
         [containerView addSubview:toView];
+        [toView addSubview:topHalfImage];
+        topHalfImage.layer.anchorPoint = CGPointMake(0.5, 0.0);
+        topHalfImage.layer.position = CGPointMake(viewSize.width / 2.0, 0.0);
+        [toView addSubview:bottomHalfImage];
+        bottomHalfImage.layer.anchorPoint = CGPointMake(0.5, 1.0);
+        bottomHalfImage.layer.position = CGPointMake(viewSize.width / 2.0, viewSize.height);
         [UIView animateWithDuration:duration delay:0.0
-             usingSpringWithDamping:1.0 initialSpringVelocity:0.1
                             options:0 animations:^{
-                                toView.frame = [context finalFrameForViewController:toViewController];
+                                static const float perspectiveFactor = -1/500.0;
+                                CATransform3D rotate3DUp = CATransform3DMakeRotation(M_PI_2, 1.0, 0.0, 0.0);
+                                rotate3DUp.m34 = perspectiveFactor;
+                                topHalfImage.layer.transform = rotate3DUp;
+                                CATransform3D rotate3DDown = CATransform3DMakeRotation(-M_PI_2, 1.0, 0.0, 0.0);
+                                rotate3DDown.m34 = perspectiveFactor;
+                                bottomHalfImage.layer.transform = rotate3DDown;
                             } completion:^(BOOL finished) {
+                                [topHalfImage removeFromSuperview];
+                                [bottomHalfImage removeFromSuperview];
                                 [context completeTransition:YES];
                             }];
     } else {
-        toView.frame = [context finalFrameForViewController:toViewController];
         [containerView addSubview:toView];
     }
 }
@@ -79,7 +100,6 @@ static const NSTimeInterval duration = 0.4;
         CGRect endFrame = CGRectMake(0.0, -containerView.frame.size.height, containerView.frame.size.width, containerView.frame.size.height);
         [containerView insertSubview:toView belowSubview:fromView];
         [UIView animateWithDuration:duration delay:0.0
-             usingSpringWithDamping:1.0 initialSpringVelocity:0.1
                             options:0 animations:^{
                                 fromView.frame = endFrame;
                             } completion:^(BOOL finished) {
