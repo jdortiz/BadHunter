e957f7a2fe4e6f63161e9f93511d996e555a7af3
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-14 23:48:57 +0200

    Draw simple shapes.

--- BadHunter/CanvasView.m
+++ BadHunter/CanvasView.m
@@ -6,16 +6,38 @@
 //  Copyright (c) 2015 PoWWaU. All rights reserved.
 //
 
+
 #import "CanvasView.h"
+
 
 @implementation CanvasView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+
+    CGRect firstRectangle = CGRectInset(rect, 5.0, 5.0);
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRect:firstRectangle];
+    [[UIColor purpleColor] setFill];
+    [bezierPath fill];
+
+    CGRect secondRect = CGRectInset(firstRectangle, 5.0, 5.0);
+    bezierPath = [UIBezierPath bezierPathWithRoundedRect:secondRect cornerRadius:5.0];
+    [[UIColor redColor] setFill];
+    [bezierPath fill];
+
+    CGRect thirdRect = CGRectOffset(CGRectInset(secondRect, 5.0, 5.0), 35.0, 0.0);
+    bezierPath = [UIBezierPath bezierPathWithOvalInRect:thirdRect];
+    [[UIColor orangeColor] setFill];
+    [bezierPath fill];
+
+    bezierPath = [UIBezierPath bezierPath];
+    [bezierPath moveToPoint:CGPointMake(CGRectGetWidth(rect)/2.0, CGRectGetHeight(rect)/2.0)];
+    [bezierPath addLineToPoint:CGPointMake(2.0, 2.0)];
+    [bezierPath addLineToPoint:CGPointMake(CGRectGetHeight(rect)-2.0, 2.0)];
+    [bezierPath addLineToPoint:CGPointMake(CGRectGetWidth(rect)/2.0, CGRectGetHeight(rect)/2.0)];
+    [[UIColor blueColor] setStroke];
+    [bezierPath stroke];
+
+
 }
-*/
 
 @end
