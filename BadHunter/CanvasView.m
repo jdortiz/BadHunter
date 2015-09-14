a03e02c6cd55421f485a6d822088f884b72c8e28
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-15 01:45:54 +0200

    Use CGContext directly.

--- BadHunter/CanvasView.m
+++ BadHunter/CanvasView.m
@@ -19,18 +19,36 @@ static const NSUInteger pointsPentagon = 5;
 #pragma mark - Drawing
 
 - (void)drawRect:(CGRect)rect {
-    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
-    UIBezierPath *circle;
-    [bezierPath moveToPoint:[self calculatePolygonPoint:0 totalPoints:pointsPentagon radius:CGRectGetWidth(rect)/2.0]];
-    [[UIColor darkGrayColor] setFill];
+
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    CGMutablePathRef pentagon = CGPathCreateMutable();
+    CGPoint initialPoint = [self calculatePolygonPoint:0 totalPoints:pointsPentagon radius:(CGRectGetWidth(rect)/2.0 - 1.0)];
+    CGPathMoveToPoint(pentagon, NULL, initialPoint.x, initialPoint.y);
     for (NSUInteger i = 1; i <= pointsPentagon; i++) {
         CGPoint corner = [self calculatePolygonPoint:i totalPoints:pointsPentagon radius:(CGRectGetWidth(rect)/2.0 - 1.0)];
-        [bezierPath addLineToPoint:corner];
-        circle = [UIBezierPath bezierPathWithArcCenter:corner radius:4.0 startAngle:0.0 endAngle:2.0*M_PI clockwise:YES];
-        [circle fill];
+        CGPathAddLineToPoint(pentagon, NULL, corner.x, corner.y);
     }
-    [[UIColor colorWithHue:352.0/360.0 saturation:1.0 brightness:0.77 alpha:1.0] setStroke];
-    [bezierPath stroke];
+    CGContextAddPath(context, pentagon);
+    CGPathRelease(pentagon);
+    CGContextSetLineWidth(context, 2.0);
+    CGContextSetStrokeColorWithColor(context, [[UIColor colorWithHue:352.0/360.0 saturation:1.0 brightness:0.77 alpha:1.0] CGColor]);
+
+    CGContextStrokePath(context);
+
+
+//    UIBezierPath *bezierPath = [UIBezierPath bezierPath];
+//    UIBezierPath *circle;
+//    [bezierPath moveToPoint:];
+//    [[UIColor darkGrayColor] setFill];
+//    for (NSUInteger i = 1; i <= pointsPentagon; i++) {
+//        CGPoint corner = [self calculatePolygonPoint:i totalPoints:pointsPentagon radius:(CGRectGetWidth(rect)/2.0 - 1.0)];
+//        [bezierPath addLineToPoint:corner];
+//        circle = [UIBezierPath bezierPathWithArcCenter:corner radius:4.0 startAngle:0.0 endAngle:2.0*M_PI clockwise:YES];
+//        [circle fill];
+//    }
+//    [[UIColor colorWithHue:352.0/360.0 saturation:1.0 brightness:0.77 alpha:1.0] setStroke];
+//    [bezierPath stroke];
 }
 
 
